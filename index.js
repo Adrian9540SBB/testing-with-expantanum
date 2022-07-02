ffefa0c6885c146ac6b3fbc07f7e7da3ab5f3a9f
Adrian9540YTexe <adrian9540gamez@gmail.com>
2022-07-02 16:20:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ window.addEventListener('load', (event)  => {
         document.getElementById("text").innerHTML = formval("Letters",powa,2,2)
     }
     const stormy = document.getElementById("btn")
-    powa = ExpantaNum("1e10e309")
+    powa = ExpantaNum("10^^^10")
     document.getElementById("text").innerHTML = formval("Letters",powa,2,2)
 
 });
